bd585b21a0611b16be73050fd3d24098013c65aa
Miguel Lopez <132926928+MiguelKLopez@users.noreply.github.com>
2023-10-08 19:06:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 import ListaTareas from './componentes/Lista-tareas';
-import logo from './img/gitlab.png';
 
 function App() {
   return (
     <div className="Aplicaciòn-Tareas">
-      <div className='logo-contenedor'>
-      <img
-          src={logo}
-          className='logo'
-          alt='logo'
-        />
       </div>
       <div className='tareas-lista-principal'>
         <h1>Mis tareas</h1>
